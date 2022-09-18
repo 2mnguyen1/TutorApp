c68be542eb02abbb689e1d7f7a6797a28e7b1df9
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2022-09-17 19:53:21 -0700

    adding intro to home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -8,9 +8,23 @@ const Home = () => {
         <div className="home-container">
             <div className="background-bubble1"></div>
             <div className="background-bubble2"></div>
-
             <div className="home-top">
                 <Navbar />
+            </div>
+            <div className="home-intro">
+                <div className="home-intro-content">
+                    <div className="home-intro-title">
+                        Make your grade <span>better</span> with us
+                    </div>
+                    <div className="home-intro-desc">
+                        We are here to help enhancing your skills and provides
+                        our best service to help you with homework and work
+                        load.
+                    </div>
+                    <button className="navbar-button">
+                        Get Started
+                    </button>
+                </div>
             </div>
         </div>
     );
