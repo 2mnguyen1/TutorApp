bae921805b5fae7b39604f5dce84b8ffd6ecce20
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2023-06-19 23:28:39 -0700

    adding tutor info page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import "./app.scss";
 import Home from "./pages/home/Home";
 import Subject from "./components/subject/Subject";
 import Navbar from "./components/navbar/Navbar";
+import Tutor from "./components/tutor/Tutor";
 
 const App = () => {
     return (
         <>
             {/* <Home /> */}
             <Navbar />
-            <Subject />
+            {/* <Subject /> */}
+            <Tutor />
         </>
     );
 };
--- client/src/components/tutor/Tutor.jsx
+++ client/src/components/tutor/Tutor.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import "./tutor.scss";
+import { Star, MilitaryTech, Groups3, AccessTime } from "@mui/icons-material";
 
-export default function tutor() {
+export default function Tutor() {
     return (
-        <div>
-            <div className="tutor-container">
+        <div className="tutor-container">
+            <div className="tutor-wrapper">
                 <h1 className="tutor-title">Tutor</h1>
-                <h2 className="tutor-name">
-                    Minh Nguyen <span className="tutor-role">Math Tutor</span>
-                </h2>
+                <h2 className="tutor-name">Minh Nguyen</h2>
+                <div className="tutor-role">Math Tutor</div>
                 <div className="tutor-rating">
                     <div className="tutor-image"></div>
                     <div className="tutor-infor">
-                        <div className="tutor-infor-rating">5.0 Rating</div>
-                        <div className="tutor-">999 Reviews</div>
-                        <div className="tutor-students">1,000,000 Students</div>
-                        <div className="tutor-hours">500 hours of tutoring</div>
+                        <div className="tutor-infor-rating">
+                            <Star /> 5.0 Rating
+                        </div>
+                        <div className="tutor-reviews">
+                            <MilitaryTech />
+                            999 Reviews
+                        </div>
+                        <div className="tutor-students">
+                            <Groups3 />
+                            1,000,000 Students
+                        </div>
+                        <div className="tutor-hours">
+                            <AccessTime />
+                            500 hours of tutoring
+                        </div>
                     </div>
                 </div>
                 <div className="tutor-description">
@@ -29,11 +40,73 @@ export default function tutor() {
                 </div>
                 <div className="tutor-student-review">
                     <div className="tutor-student-rating">
-                        Student Reviews 5.0
+                        <span>Student Reviews</span>{" "}
+                        <span className="stars">
+                            5.0 <Star /> <Star /> <Star /> <Star /> <Star />
+                        </span>
                     </div>
                     <div className="tutor-student-comment">
-                        <div className="students-comments"></div>
-                        <div className="student-reviews"></div>
+                        <div className="student-comment-wrapper">
+                            <div className="students-comments">
+                                <div className="student-comment">
+                                    <div className="student-icon">
+                                        <img />
+                                    </div>
+                                    <div className="student-contents">
+                                        <div className="student-rating-history">
+                                            <div className="student-name">
+                                                Minh Nguyen
+                                            </div>
+                                            <div className="student-rating">
+                                                <Star /> <Star /> <Star />{" "}
+                                                <Star /> <Star />
+                                            </div>
+                                        </div>
+                                        <div className="student-words">
+                                            Wow! He is awesome
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                            </div>
+                            <div className="students-comments">
+                                <div className="student-comment">
+                                    <div className="student-icon">
+                                        <img />
+                                    </div>
+                                    <div className="student-contents">
+                                        <div className="student-rating-history">
+                                            <div className="student-name">
+                                                Minh Nguyen
+                                            </div>
+                                            <div className="student-rating">
+                                                <Star /> <Star /> <Star />{" "}
+                                                <Star /> <Star />
+                                            </div>
+                                        </div>
+                                        <div className="student-words">
+                                            Wow! He is awesome
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                            </div>
+                        </div>
+                        <div className="student-reviews">
+                            <div className="student-comment-stars">
+                                <div>Leave a review: </div>
+                                <span>
+                                    {" "}
+                                    <Star /> <Star /> <Star /> <Star /> <Star />
+                                </span>
+                            </div>
+                            <div className="student-texts">
+                                <textarea></textarea>
+                            </div>
+                            <button className="student-submit-btn">
+                                Submit
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
